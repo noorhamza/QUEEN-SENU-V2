cc10629bdb9e9b0ca286ad166d96f9d00af374f5
noorhamza <86974133+noorhamza@users.noreply.github.com>
2025-02-17 22:26:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'KIgknJqS#ta_8waLqM5V35eIYeX1Cl7xk4PA0fRaJSQ5BjYZVCDk',
+    SESSION_ID: process.env.SESSION_ID || '7dpSiArC#O3sqDzNtwViI4ODjTuXNsH7BiG_ouO5UiC49V54-_wQ',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
